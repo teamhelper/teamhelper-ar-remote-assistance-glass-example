f49dc2a23e1b9f674043908c75ebfd5208f06ab2
ycl-mst <158993340+ycl-mst@users.noreply.github.com>
2024-03-04 11:59:27 +0800

    Update Config.java

--- app/src/main/java/com/teamhelper/glass/Config.java
+++ app/src/main/java/com/teamhelper/glass/Config.java
@@ -1,7 +1,7 @@
 package com.teamhelper.glass;
 
 public interface Config {
-    String APP_KEY = "VOJ170mWRKxyY27H0BQKz22y1XKfbP1m";
-    String ACCESS_KEY = "AK4PoiuLSiA54otVhyVg9Ssm";
-    String ACCESS_SECRET = "Yt7Myvkymrm3cbcp0zWn0N5H6ZTpKR";
+    String APP_KEY = "";
+    String ACCESS_KEY = "";
+    String ACCESS_SECRET = "";
 }
